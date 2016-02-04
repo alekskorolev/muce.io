32f67a3e2ef21e396a54564f287c5d4e1c1c3b53
Aleksander Korolev <a_korolev@wargaming.net>
2016-02-04 18:41:03 +0300

    add simple component who only render

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import BaseView from './lib/base_view';
 import BaseModel from './lib/base_model';
 import BaseCollection from './lib/base_collection';
 import BaseComponent from './lib/base_component';
+import BaseSimpleComponent from './lib/base_simple_component';
 import BaseRouter from './lib/base_router';
 import ComponentsView from './lib/components';
 
@@ -18,7 +19,7 @@ var components,
 	startHistory;
 
 components = new ComponentsView();
-window.snpcInit = _.bind(components.initComponent, components);
+window.jscInit = _.bind(components.initComponent, components);
 startHistory = _.bind(history.start, history);
 
 export default components;
@@ -31,6 +32,7 @@ export {
     BaseModel,
     BaseView,
     BaseRouter,
+    BaseSimpleComponent,
     Storage,
     startHistory
 }
