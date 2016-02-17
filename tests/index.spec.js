969578051b941d7674c5f7ded4badf66cbf518d7
Aleksander Korolev <a_korolev@wargaming.net>
2016-02-17 13:37:22 +0300

    add base tests

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,4 +1,6 @@
 import {
+    _,
+    $,
     BaseCollection,
     BaseComponent,
     BaseModel,
@@ -14,5 +16,14 @@ import {expect} from 'chai';
 describe('Main script import and build', () => {
     it('should be have imported libs', () => {
         expect(BaseCollection).to.be.a('function');
+        expect(BaseComponent).to.be.a('function');
+        expect(BaseModel).to.be.a('function');
+        expect(BaseView).to.be.a('function');
+        expect(BaseRouter).to.be.a('function');
+        expect(BaseSimpleComponent).to.be.a('function');
+        expect(Storage).to.be.a('function');
+        expect(startHistory).to.be.a('function');
+        expect(_.extract).to.be.a('function');
+        expect(_.mixed).to.be.a('function');
     });
 });
